2085e389932f54adddd5ed5bb4c053ece0e9f5e7
Roberto Junior <robertojuniorazul@gmail.com>
2025-02-15 14:25:51 -0300

    Foi adicionado o react-hook-form no componente CreateTask

--- frontEnd/src/components/CreateTask.tsx
+++ frontEnd/src/components/CreateTask.tsx
@@ -1,15 +1,71 @@
+import { useForm } from "react-hook-form";
 import { CreateTaskContainer } from "./CreateTask.styles";
 
+interface Task {
+    titulo: string;
+    descricao: string;
+    status: 'PENDENTE' | 'EM_ANDAMENTO' | 'FEITO';
+  }
+
+
 export function CreateTask () {
+    const { register, handleSubmit, reset } = useForm<Task>();
+    const url = 'http://localhost:3333/task';
+
+    function submit(data: Task) {
+        // Dados a serem enviados
+        const newTask = {
+        titulo: data.titulo,
+        descricao: data.descricao,
+        status: data.status
+        };
+
+        // Configuração da requisição POST
+        fetch(url, {
+        method: 'POST', // Método da requisição
+        headers: {
+            'Content-Type': 'application/json' // Especifica que o conteúdo está em JSON
+        },
+        body: JSON.stringify(newTask) // Converte o objeto data em uma string JSON
+        })
+        .then(response => response.json()) // Converte a resposta para JSON
+        .then(data => {
+            console.log('Sucesso:', data); // Exibe os dados retornados
+        })
+        .catch(error => {
+            console.error('Erro:', error); // Exibe erros caso ocorram
+        });
+
+
+        // console.log(data)
+
+
+        reset()
+    }
+
     return (
-        <CreateTaskContainer>
+        <CreateTaskContainer onSubmit={handleSubmit(submit)}>
             <h2>Crie uma nova tarefa</h2>
             <div className="optionsContainer">
                 <div className="inputsContainer">
-                    <input type="text" placeholder="Titulo"/>
-                    <input type='text' name="" id="" placeholder="Descrição"/>
+                    <input 
+                        type="text" 
+                        placeholder="Titulo"
+                        {...register("titulo", {required: true})}
+                    />
+
+                    <input 
+                        type='text' 
+                        id="" 
+                        placeholder="Descrição"
+                        {...register("descricao", {required: true})}
+                    />
                 </div>
-                <select name="status" id="" className='selectContainer'>
+                <select 
+                    id="" 
+                    className='selectContainer'
+                    {...register("status", {required: true})}
+                >
                     <option value="PENDENTE">PENDENTE</option>
                     <option value="EM_ANDAMENTO">EM ANDAMENTO</option>
                     <option value="FEITO">FEITO</option>
